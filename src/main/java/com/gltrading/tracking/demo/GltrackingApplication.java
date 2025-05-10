aeab5b626da2d7b6edecf4240f89ad38a91c31e9
galongatti <galongatti@hotmail.com>
2025-05-10 15:39:14 -0300

    fist commit

---
+++ src/main/java/com/gltrading/tracking/demo/GltrackingApplication.java
@@ -0,0 +1,15 @@
+package com.gltrading.tracking.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GltrackingApplication {
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(GltrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gltrading/tracking/demo/GltrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gltrading.tracking.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GltrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
